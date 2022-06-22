4a68ca7ca565c92e221c4ced2f127e02cf52e75e
jiejenn@learndataanalysis.org <jiejenn@learndataanalysis.org>
2022-06-22 11:52:34 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=['youtube'],
     author='Jie Jenn',
     author_email='jiejenn@learndataanalysis.org',
-    version='1.0',
+    version='1.0.1',
     keywords=['YouTube', 'YouTube Scraper'],
     python_requires='>=3.6',
     install_requires=['google-auth>=1.12.0', 'google-auth-oauthlib>=0.4.1'],
