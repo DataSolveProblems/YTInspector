c5c6e6af6e0c3dc04171f71c9b8379b358ece9ae
jiejenn@learndataanalysis.org <jiejenn@learndataanalysis.org>
2022-06-22 11:40:41 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import re
+import sys
+from pathlib import Path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='youtube',
+    packages=['youtube'],
+    author='Jie Jenn',
+    author_email='jiejenn@learndataanalysis.org',
+    version='1.0',
+    scripts=['first.py'],
+    keywords=['YouTube', 'YouTube Scraper'],
+    python_requires='>=3.6',
+    install_requires=['google-auth>=1.12.0', 'google-auth-oauthlib>=0.4.1'],
+    license='MIT'
+)    
