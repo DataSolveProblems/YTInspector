6cf966a5a28946b58535016b46a171df06745664
jiejenn@learndataanalysis.org <jiejenn@learndataanalysis.org>
2022-06-24 16:14:41 +0800

    update __init__.py

--- ytinspector/__init__.py
+++ ytinspector/__init__.py
@@ -1,7 +1,8 @@
 
 from .exceptions import (YouTubeException, NoVideosReturned, NoCommentsReturned, SearchResultReturnsNone)
-from .youtube import YouTube, convert_duration
+from .youtube import YouTube
 from .google_apis import create_service, convert_to_RFC_datetime
+from .utility import convert_duration, locate_channel_id
 
 __author__ = 'Jie Jenn'
 __version__ = '1.0.3'
