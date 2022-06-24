5bcc0fd6ba9e6819b2a2f610cfefbbc04c43bd49
jiejenn@learndataanalysis.org <jiejenn@learndataanalysis.org>
2022-06-24 16:13:58 +0800

    add locate_channel_id func

--- ytinspector/utility.py
+++ ytinspector/utility.py
@@ -1,3 +1,4 @@
+import re
 from collections import namedtuple
 
 ChannelRelatedComment = namedtuple('VideoComment', 
@@ -20,3 +21,26 @@ SearchResultsChannel = namedtuple('SearchResultsChannel',
 SearchResultsPlaylist = namedtuple('SearchResultsPlaylist',
     ['playlist_id', 'playlist_title', 'channel_id', 'channel_title', 'description', 'published_at', 'region_code', 'thumbnails', 'playlist_url']
 )
+
+
+def convert_duration(duration):
+	"""
+	Convert duration string to seconds
+	"""
+	try:
+		h = int(re.search('\d+H', duration)[0][:-1]) * 60**2  if re.search('\d+H', duration) else 0
+		m = int(re.search('\d+M', duration)[0][:-1]) * 60  if re.search('\d+M', duration) else 0
+		s = int(re.search('\d+S', duration)[0][:-1])  if re.search('\d+S', duration) else 0
+		return h + m + s
+	except Exception as e:
+		print(e)
+		return 0
+
+def locate_channel_id(video_id):
+    """returns channel url and channel id"""
+    response = requests.get('https://www.youtube.com/watch?v={0}'.format(video_id))
+    results = re.search('http://www.youtube.com/channel/(\w+)', response.text, re.MULTILINE)
+    if results:
+        return results.group(0), results.group(1)
+    else:
+        return
